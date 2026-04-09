0851204cb2bd6f5f86702f0dda122056e8131707
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-04-09 22:36:16 +0300

    search word line by line

--- 1.go-reloaded/main.go
+++ 1.go-reloaded/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	data, err := os.ReadFile("quiz.txt")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error:", err)
+		return
 	}
-	fmt.Printf(string(data))
+
+	content := string(data)
+lines := strings.Split(content, "\n")
+
+for i, line := range lines {
+	if strings.Contains(line, "Go") {
+		fmt.Println("Found in line", i+1, ":", line)
+	}
+}
 }
