dd406318aca68b8746bf30e763c6748b62cc9f47
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-06-15 18:49:20 +0300

    server to print data on the webpage

--- 6.groupie-tracker/cmd/server/main.go
+++ 6.groupie-tracker/cmd/server/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
     "fmt"
+    "net/http"
     "groupie-tracker/internal/api"
 )
 
-func main() {
+func home(w http.ResponseWriter, r *http.Request) {
     artists, err := api.GetArtists()
     if err != nil {
-        fmt.Println("Error:", err)
+        http.Error(w, "Failed to load artists", http.StatusInternalServerError)
         return
     }
 
     for _, a := range artists {
-        fmt.Printf("ID:%d  Name:%-25s  Founded:%d  Members:%v\n",
-            a.ID, a.Name, a.CreationDate, a.Members)
+        fmt.Fprintf(w, "ID:%d  Name:%-25s  Founded:%d\n", a.ID, a.Name, a.CreationDate)
     }
+}
+
+func main() {
+    http.HandleFunc("/", home)
+
+    fmt.Println("Server running at http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
 }
